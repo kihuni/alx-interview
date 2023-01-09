e7888170dc0537180b4c7e8306b6b9c829d814be
stephen kihuni <stephenkihuni55@gmail.com>
2023-01-09 06:39:56 +0300

    modified the function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,38 @@
 """Create a function def pascal_triange(n) that returns a list of lists of integers"""
 
 def pascal_triangle(n):
-    """Create an array of list"""
-    lists = []
+
+    """check if the input is less than or equal to 0, if it's, empty list is returned"""
+    if n<=0:
+        return []
+
+    """otherwise the function initializes a list called lists with n numbers of 0s"""
+    lists = [0]*n
 
     """iterate through n numbers"""
-    for row in range(n):
-        """if n <=0 return an empty list"""
-        if n <=0:
-            lists.append([])
-        else:
-            """return the first row"""
-            new_row = [1]
-        for i in range(row-1):
-            new_row.append(lists[row-1][i]+lists[row-1][i+1])
-            new_row.append(1)
-            lists.append(new_row)
-        return lists
+    for i in range(n):
+
+        """On each iteration, a new list is called new_row is created and initialised with `i+1 0s"""
+        new_row = [0]*(i+1)
+
+        """fill first index with 1"""
+        new_row[0] = 1
+
+        """fill last index with 1""" 
+        new_row[len(new_row)-1] =1
+
+        """Inner iteration from 1 to i-1"""
+        for j in range(1, i+1):
+
+                """Calcuate the sum of two numbers  in the previous row"""      
+                a = lists[i-1][j]
+                b = lists[i-1][j-1]
+
+                """assigns the sum to the current potion in new_row"""
+                new_row[j] = a+b
+
+        """Finally assigns new_row to the current position in the lists"""
+        lists[i] = new_row
+        """return lists"""
+    return lists
 
---
+++ READ.md
@@ -0,0 +1,3 @@
+Pascal Triangle
+
+Pascal's Triangle is a triangular arrangement of numbers in which the first row is 1, and the remaining rows are formed by adding the two numbers above a position in the previous row
