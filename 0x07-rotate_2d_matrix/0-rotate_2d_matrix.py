75198d6a2888007f4492fc22184254418839a4bd
stephen kihuni <stephenkihuni55@gmail.com>
2023-02-19 05:08:54 +0300

    added 2d matrix task

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    
+    # Transpose the matrix "Swapping its rows with colums"
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+
+    for i in range(n):
+        for j in range(n//2):
+            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
